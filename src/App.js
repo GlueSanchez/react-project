61b786eaf1eb2fa27daf9b466aa0aea4f955223c
GlueSanchez <visaszanew@gmail.com>
2020-12-07 13:05:47 +0200

    Added Products page

--- src/App.js
+++ src/App.js
@@ -2,14 +2,157 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from "react";
 import Header from "./components/Header/Header";
+import ProductsComponent from "./components/Products/ProductsComponent";
 
 function App() {
     return (
-        <div className='bgContainer'>
-            <div className="mainContainer">
-                <Header/>
+        <>
+            <div className='bgContainer'>
+                <div className="mainContainer">
+                    <Header/>
+                </div>
+
             </div>
-        </div>
+            <div className='mainContainer'>
+                <ProductsComponent/>
+
+                <div style={{
+                    display: 'grid',
+                    gridTemplateColumns: 'repeat(5,1fr)',
+                    justifyItems: 'center',
+                    borderTop: '1px solid black',
+                    borderRight: '1px solid black',
+                }}>
+                    <div style={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        padding: '50px 15px',
+                        textAlign: "center",
+                        width: '100%',
+                        borderLeft: '1px solid black',
+                        borderBottom: '1px solid black',
+                    }}>
+                        <img src="https://pbs.twimg.com/media/DiHYZjOVAAA95Yc.jpg"
+                             alt=""
+                             width={75}
+                             height={75}/>
+                        <span style={{
+                            paddingBottom: '10px',
+                            maxWidth: '75px',
+                            marginTop: '20px'
+
+                        }}>Old Skool Shoes</span>
+                        <span style={{
+                            marginTop: '10px'
+                        }}> $59.99</span>
+                    </div>
+                    <div style={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        padding: '50px 15px',
+                        textAlign: "center",
+                        width: '100%',
+                        borderLeft: '1px solid black',
+                        borderBottom: '1px solid black',
+                    }}>
+                        <img src="https://pbs.twimg.com/media/DiHYZjOVAAA95Yc.jpg"
+                             alt=""
+                             width={75}
+                             height={75}/>
+                        <span style={{
+                            paddingBottom: '10px',
+                            maxWidth: '75px',
+                            marginTop: '20px'
+
+                        }}>Old Skool Shoes</span>
+                        <span style={{
+                            marginTop: '10px'
+                        }}> $59.99</span>
+                    </div>
+                    <div style={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        padding: '50px 15px',
+                        textAlign: "center",
+                        width: '100%',
+                        borderLeft: '1px solid black',
+                        borderBottom: '1px solid black',
+                    }}>
+                        <img src="https://pbs.twimg.com/media/DiHYZjOVAAA95Yc.jpg"
+                             alt=""
+                             width={75}
+                             height={75}/>
+                        <span style={{
+                            paddingBottom: '10px',
+                            maxWidth: '75px',
+                            marginTop: '20px'
+
+                        }}>Old Skool Shoes</span>
+                        <span style={{
+                            marginTop: '10px'
+                        }}> $59.99</span>
+                    </div>
+                    <div style={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        padding: '50px 15px',
+                        textAlign: "center",
+                        width: '100%',
+                        borderLeft: '1px solid black',
+                        borderBottom: '1px solid black',
+                    }}>
+                        <img src="https://pbs.twimg.com/media/DiHYZjOVAAA95Yc.jpg"
+                             alt=""
+                             width={75}
+                             height={75}/>
+                        <span style={{
+                            paddingBottom: '10px',
+                            maxWidth: '75px',
+                            marginTop: '20px'
+
+                        }}>Old Skool Shoes</span>
+                        <span style={{
+                            marginTop: '10px'
+                        }}> $59.99</span>
+                    </div>
+                    <div style={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        padding: '50px 15px',
+                        textAlign: "center",
+                        width: '100%',
+                        borderLeft: '1px solid black',
+                        borderBottom: '1px solid black',
+                    }}>
+                        <img src="https://pbs.twimg.com/media/DiHYZjOVAAA95Yc.jpg"
+                             alt=""
+                             width={75}
+                             height={75}/>
+                        <span style={{
+                            paddingBottom: '10px',
+                            maxWidth: '75px',
+                            marginTop: '20px'
+
+                        }}>Old Skool Shoes</span>
+                        <span style={{
+                            marginTop: '10px'
+                        }}> $59.99</span>
+                    </div>
+                </div>
+
+            </div>
+        </>
+
     );
 }
 
--- src/components/Header/Navbar/NavbarComponent.js
+++ src/components/Header/Navbar/NavbarComponent.js
@@ -7,6 +7,7 @@ const NavbarComponent = (props) => {
         <>
             <Navbar className={c.navbar} collapseOnSelect expand="md" bg="dark" variant="dark">
                 <div className={c.opo}>
+                    <div className={c.block}>
                     <Navbar.Brand>
                         <img
                             src="https://steamuserimages-a.akamaihd.net/ugc/849347535113279721/AF826A5BD75A44D4198BFB14E3C451ECE98097FA/"
@@ -17,8 +18,9 @@ const NavbarComponent = (props) => {
                             className={'d-inline-block align-top'}/>
                     </Navbar.Brand>
                     <Navbar.Toggle aria-controls={'responsive-navbar-nav'}/>
+                    </div>
                     <Navbar.Collapse className={c.navbarItems} id="responsive-navbar-nav">
-                        <Nav>
+                        <Nav className={c.navbarItem}>
                             <Nav.Link href="#features">Features</Nav.Link>
                             <Nav.Link href="#pricing">Pricing</Nav.Link>
                             <Nav.Link href="#deets">More deets</Nav.Link>
