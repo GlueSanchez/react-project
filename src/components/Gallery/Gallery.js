cfdcedeef3b10f9975a872b188b7572162f991c6
GlueSanchez <visaszanew@gmail.com>
2021-02-09 19:48:59 +0200

    'upd'

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -1,9 +1,23 @@
 import React from 'react';
-
+import c from './Gallery.module.css';
+import GalleryItem from "./GalleryItems/GalleryItem";
 const Gallery = (props) => {
     return (
         <div>
-123
+            <div className={c.galleryWrapper}>
+                <div className={c.galleryTitle}>Gallery</div>
+                <div className={c.galleryItems}>
+                    <GalleryItem />
+                    <GalleryItem />
+                    <GalleryItem />
+                    <GalleryItem />
+                    <GalleryItem />
+                    <GalleryItem />
+                    <GalleryItem />
+                    <GalleryItem />
+                    <GalleryItem />
+                </div>
+            </div>
         </div>
     );
 }
