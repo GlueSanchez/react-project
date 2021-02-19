6b1baf7710aba6907dc52bd738b38a7c030378bc
GlueSanchez <visaszanew@gmail.com>
2021-02-19 14:32:58 +0200

    added new features to gallery

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -1,21 +1,55 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import c from './Gallery.module.css';
 import GalleryItem from "./GalleryItems/GalleryItem";
+
 const Gallery = (props) => {
+    const [currentImg, setCurrentImg] = useState({
+        photoIndex: 0,
+        isOpen: false
+    });
+    const getIndexOfCurrentImg = (index) => {
+        setCurrentImg(prevState => ({...prevState, isOpen: true, photoIndex: index}));
+    }
+    const handleClick = () => {
+        // setCurrentImg(prevState => ({...prevState, isOpen: false}));
+    }
+
+    const closeModal = () => {
+        setCurrentImg(prevState => ({...prevState, isOpen: false}));
+    }
+    const changeImgToPrevious = () => {
+        setCurrentImg(prevState => ({...prevState, photoIndex: currentImg.photoIndex - 1}));
+    }
+    const changeImgToNext = () => {
+        setCurrentImg(prevState => ({...prevState, photoIndex: currentImg.photoIndex + 1}));
+    }
+    useEffect(() => {
+        console.log(currentImg)
+    })
     return (
         <div>
             <div className={c.galleryWrapper}>
                 <div className={c.galleryTitle}>Gallery</div>
+                {currentImg.isOpen ?
+                    <div className={c.modal}
+                         onClick={handleClick}
+                         style={{backgroundImage: `url('https://wgntv.com/wp-content/uploads/sites/5/2018/08/gettyimages-1006480588-1.jpg?w=760')`}}>
+                        <span className={c.galleryItemNumber}>{currentImg.photoIndex}</span>
+                        <button className={c.arrow + ' ' + c.prevArrow} onClick={changeImgToPrevious}> {'<'} </button>
+                        <button className={c.arrow + ' ' + c.nextArrow} onClick={changeImgToNext}> ></button>
+                        <button className={c.arrow + ' ' + c.close} onClick={closeModal}>Close</button>
+
+                    </div> : <></>}
                 <div className={c.galleryItems}>
-                    <GalleryItem />
-                    <GalleryItem />
-                    <GalleryItem />
-                    <GalleryItem />
-                    <GalleryItem />
-                    <GalleryItem />
-                    <GalleryItem />
-                    <GalleryItem />
-                    <GalleryItem />
+                    <GalleryItem id={0} getIndexOfCurrentImg={getIndexOfCurrentImg}/>
+                    <GalleryItem id={1} getIndexOfCurrentImg={getIndexOfCurrentImg}/>
+                    <GalleryItem id={2} getIndexOfCurrentImg={getIndexOfCurrentImg}/>
+                    <GalleryItem id={3} getIndexOfCurrentImg={getIndexOfCurrentImg}/>
+                    <GalleryItem id={4} getIndexOfCurrentImg={getIndexOfCurrentImg}/>
+                    <GalleryItem id={5} getIndexOfCurrentImg={getIndexOfCurrentImg}/>
+                    <GalleryItem id={6} getIndexOfCurrentImg={getIndexOfCurrentImg}/>
+                    <GalleryItem id={7} getIndexOfCurrentImg={getIndexOfCurrentImg}/>
+                    <GalleryItem id={8} getIndexOfCurrentImg={getIndexOfCurrentImg}/>
                 </div>
             </div>
         </div>
--- src/components/Gallery/GalleryItems/GalleryItem.js
+++ src/components/Gallery/GalleryItems/GalleryItem.js
@@ -2,22 +2,16 @@ import React from 'react';
 import c from "../Gallery.module.css";
 
 const GalleryItem = (props) => {
-    const handleClick = (e) => {
 
-        e.target.lastElementChild.style.display = 'flex';
-    }
-    const handle = (e) => {
-        let none = 'none';
-        e.target.style.display = none;
+    const handleClick = () => {
+        props.getIndexOfCurrentImg(props.id);
     }
     return (
         <div className={c.galleryItem}
              onClick={handleClick}
-             style={{backgroundImage: `url('https://lh3.googleusercontent.com/proxy/VP-9jP0n0XxBlc_PMwXxxuBNUlBmbF2TkNxXbCo2t1kK8w5EXoOIJDHo_q_DdYKIX6gkeymwnJgz-IOCu0CWOv3ITOQHtv2RRjkzD0gflvGd')`}}>
-            <span className={c.galleryItemNumber}>1</span>
-            <div className={c.modal}
-                 onClick={handle}>12
-            </div>
+             style={{backgroundImage: `url('https://wgntv.com/wp-content/uploads/sites/5/2018/08/gettyimages-1006480588-1.jpg?w=760')`}}>
+            <span className={c.galleryItemNumber}>{props.id}</span>
+
         </div>
     );
 }
