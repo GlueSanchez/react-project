77156bc4416bf535fca8e6f292d1dec6b5699f21
GlueSanchez <visaszanew@gmail.com>
2021-05-24 22:48:42 +0300

    fix bag

--- src/App.js
+++ src/App.js
@@ -2,26 +2,15 @@ import './App.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from "react";
 import {Route, BrowserRouter} from 'react-router-dom';
-import Login from "./components/Login/Login";
-import Header from "./components/Header/Header";
-import MainPage from "./components/MainPage/MainPage";
-import Articles from "./components/Articles/Articles";
-import FullArticle from "./components/Articles/FullArticle/FullArticle";
-import Gallery from "./components/Gallery/Gallery";
-import Footer from "./components/Footer/Footer";
-import GalleryOnce from "./components/Gallery/GalleryOnce/GalleryOnce";
-import {Container} from "@material-ui/core";
 import User_Page from "./components/User_page/User_Page";
 import Admin from "./components/Admin/Admin";
-import Switch from "react-bootstrap/Switch";
 
 const App = () => {
     return (
         <BrowserRouter>
-                        <Route exact path='/admin' component={Admin}/>
-                        <Route exact path='/' component={User_Page}/>
-
-</BrowserRouter>
+            <Route exact path='/admin' component={Admin}/>
+            <Route path='/' component={User_Page}/>
+        </BrowserRouter>
 
     );
 }
--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -7,6 +7,7 @@ import {Col, Image, Row} from "react-bootstrap";
 import {faArrowLeft, faArrowRight, faExternalLinkAlt, faTimes} from "@fortawesome/free-solid-svg-icons";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import SimpleReactLightbox, {SRLWrapper} from 'simple-react-lightbox'
+import {makeStyles} from "@material-ui/core/styles";
 
 const images = [
     {image: 'https://picsum.photos/200/300'},
@@ -17,84 +18,55 @@ const images = [
     {image: 'https://picsum.photos/200/306'},
     {image: 'https://picsum.photos/350/308'}
 ];
-
-
+const useStyles = makeStyles((theme) => ({
+    image: {
+        position: 'relative'
+    },
+    imageText: {
+        position: 'absolute',
+        left: '15px',
+        bottom: 0,
+        height: '25px',
+        width: '25px',
+        color: 'white',
+        backgroundColor: 'black',
+        padding: '3px',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
+}));
+const options = {
+    // Please note that "caption" is singular
+    caption: {
+        captionAlignment: 'start',
+        captionColor: '#FFFFFF',
+        captionContainerPadding: '0',
+        captionFontFamily: 'inherit',
+        captionFontSize: 'inherit',
+        captionFontStyle: 'inherit',
+        captionFontWeight: 'inherit',
+        captionTextTransform: 'inherit',
+        showCaption: true
+    }
+};
 const Gallery = (props) => {
-    // const [currentImg, setCurrentImg] = useState({
-    //     photoIndex: 0,
-    //     isOpen: false
-    // });
-    // const handlers = useSwipeable({
-    //     onSwipedLeft: () => changeImgToPrevious(),
-    //     onSwipedRight: () => changeImgToNext(),
-    //     preventDefaultTouchmoveEvent: true,
-    //     trackMouse: true
-    // });
-
-
-    // const getIndexOfCurrentImg = (index) => {
-    //     setCurrentImg(prevState => ({...prevState, isOpen: true, photoIndex: index}));
-    // }
-    // const closeModal = (e) => {
-    //     setCurrentImg(prevState => ({...prevState, isOpen: false}));
-    // }
-    // const changeImgToPrevious = (e) => {
-    //     setCurrentImg(prevState => ({...prevState, photoIndex: currentImg.photoIndex - 1}));
-    // }
-    // const changeImgToNext = (e) => {
-    //     setCurrentImg(prevState => ({...prevState, photoIndex: currentImg.photoIndex + 1}));
-    // }
+    const classes = useStyles();
 
     return (
         <SimpleReactLightbox>
-            <SRLWrapper>
+            <SRLWrapper options={options}>
                 <div className="content">
                     <div className="row">
-
-                {images.map(img => (
-                    <div className="col-md-3 col-12 col-image-half">
-                        <Image src={img.image}/>
-                    </div>
-                ))}
+                        {images.map(img => (
+                            <div className="col-md-3 col-12 col-image-half">
+                                <Image className={classes.image} src={img.image} alt="#1"/>
+                                <div className={classes.imageText}>#1</div>
+                            </div>
+                        ))}
                     </div>
                 </div>
             </SRLWrapper>
-            {/*<div {...handlers}>*/}
-            {/*    <div className={c.galleryWrapper}>*/}
-            {/*        <div className={c.galleryTitle}>Gallery</div>*/}
-            {/*        {currentImg.isOpen ?*/}
-            {/*            <div className={c.modal}>*/}
-            {/*                <TransitionGroup>*/}
-            {/*                    <CSSTransition*/}
-            {/*                        key={currentImg.photoIndex}*/}
-            {/*                        timeout={300}*/}
-            {/*                        classNames='example'>*/}
-            {/*                        <GalleryItem id={currentImg.photoIndex}*/}
-            {/*                                     photo={images[currentImg.photoIndex]}/>*/}
-            {/*                    </CSSTransition>*/}
-            {/*                </TransitionGroup>*/}
-            {/*                <button className={c.btn + ' ' + c.prevArrow + ' ' + c.arrow} onClick={changeImgToPrevious}>*/}
-            {/*                    <FontAwesomeIcon icon={faArrowLeft}/>*/}
-            {/*                </button>*/}
-            {/*                <button className={c.btn + ' ' + c.nextArrow + ' ' + c.arrow} onClick={changeImgToNext}>*/}
-            {/*                    <FontAwesomeIcon icon={faArrowRight}/>*/}
-            {/*                </button>*/}
-            {/*                <button className={c.btn + ' ' + c.close} onClick={closeModal}>*/}
-            {/*                    <FontAwesomeIcon icon={faTimes}/>*/}
-            {/*                </button>*/}
-            {/*            </div>*/}
-            {/*            : <></>}*/}
-            {/*        <div className={c.galleryItems}>*/}
-            {/*            {images.map((slide, index) => (*/}
-            {/*                <img src={slide.image}*/}
-            {/*                     className={c.galleryItem}*/}
-            {/*                     onClick={() => {*/}
-            {/*                         getIndexOfCurrentImg(index);*/}
-            {/*                     }}/>*/}
-            {/*            ))}*/}
-            {/*        </div>*/}
-            {/*    </div>*/}
-            {/*</div>*/}
         </SimpleReactLightbox>
     );
 }
